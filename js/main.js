96302a66745d5239a29c8d0fc1e84622a36b4cf7
Thomas Seals <tjseals@gmail.com>
2013-12-12 16:54:35 -0600

    added input form for adding to html page via jquery event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert('hi');
+var itemStore = $('#templates .item').html();
+$(".new-item").on('submit', function (e) {
+  var name = $('.name').val();
+  var price = $('.price').val();
+  e.preventDefault();
+  var newStore = $.render(itemStore, {
+    name: name,
+    price: price
+  });
+  $('#store').append(newStore);
+  this.reset();
+});
